a79586065ffbcced689fe16afd5f50502da06cc8
Abhishek-Jadhav-2618 <abhijadhav2618@gmail.com>
2026-02-24 17:36:08 +0530

    Added secrets

--- variables.tf
+++ variables.tf
@@ -1,23 +1,17 @@
 variable "vpc_name" {
-  default = "This is VPC Name"
 }
 
 variable "vpc_cidr" {
-  default = "This is the default value"
 }
 
 variable "public_subnet_cidr" {
-  default = "This is the default value"
 }
 
 variable "private_subnet_cidr" {
-  default = "This is the default value"
 }
 
 variable "availability_zones" {
-  default = "This is the default value"
 }
 
 variable "project_name" {
-  default = "This is the default value"
 }
